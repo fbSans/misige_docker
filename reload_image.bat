6d7523e90b9658c5a20460b1ee2410d72a0e3f42
Fabricio <fabrisansao@gmail.com>
2024-10-11 08:12:45 +0200

    Fix commands and remove old comments

--- reload_image.bat
+++ reload_image.bat
@@ -1,13 +1,11 @@
 @echo off
 setlocal
-rem Docker file ainda esta na fase inicial
 
-rem No caso de usar linux, basta copiar os comandos docker abaixo no bash script
-NAME=misige
+set NAME=misige
+
+echo %NAME%
 docker stop %NAME% 
 docker rm %NAME%
-docker build -t="%$NAME%" .  && docker run -t -i -h %NAME% -p 2024:80 --name="%NAME% %NAME%
-
-
+docker build -t="%NAME%" .  && docker run -t -i -h %NAME% -p 2024:80 --name="%NAME%" %NAME%
 endlocal
 @echo on
